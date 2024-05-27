413f7c2ea2c4ec7a068b8aed181bce039f16ac9f
marektro <m.troszczynski@selleo.com>
2024-05-27 14:38:32 +0200

    CU-8694m0phh - [SnapFlowChanges] Disable auto opening onboarding after connecting metamask

--- packages/site/src/App.tsx
+++ packages/site/src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { FC, useEffect, useMemo, useRef, useState } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import {
   GetAccountInfoResponse,
   Protocol,
@@ -38,7 +38,6 @@ const SupportedNetworks = new Map<Protocol, Protocol>([
 ]);
 
 const App: FC<{ hideLogo: () => void }> = ({ hideLogo }) => {
-  const triggerModalOnceRef = useRef(false);
   const { openOnboardingModal, setOnFinishCallback } = useOnboardingModal();
   const [metamask, metamaskDispatch] = useMetaMask();
   const rampClient = useRampClient();
@@ -201,13 +200,6 @@ const App: FC<{ hideLogo: () => void }> = ({ hideLogo }) => {
   };
 
   useEffect(() => {
-    if (
-      accountInfo?.result.case == 'authentication' &&
-      !triggerModalOnceRef.current
-    ) {
-      triggerModalOnceRef.current = true;
-    }
-
     if (accountInfo?.result.case == 'account') {
       hideLogo();
     }
