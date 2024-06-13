289fff70b39fe920382ac708cd133db21b2b11d5
tobob <w.bozek@selleo.com>
2024-06-13 12:44:16 +0200

    CU-8694a9aej - [Snap] Production readiness

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -2,10 +2,10 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import viteCompression from 'vite-plugin-compression';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), viteCompression()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
