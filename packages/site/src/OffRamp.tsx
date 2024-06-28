19851dee81de9681336567dcea22556bb5c85839
marektro <m.troszczynski@selleo.com>
2024-06-28 19:53:32 +0200

    CU-8694xwuza - [Refactoring] Extract methods from Offramp and Onramp to utils

--- packages/site/src/OffRamp.tsx
+++ packages/site/src/OffRamp.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
+import { FunctionComponent, useEffect, useRef, useState } from 'react';
 import {
   EstimateOffRampFeeRequest,
   EstimateOffRampFeeResponse,
@@ -57,6 +57,7 @@ import WarningIcon from '@/assets/warningIcon';
 import { handle32002 } from '@/lib/utils';
 import { Wallet } from '@/components/AddWallet';
 import { handleInput } from './utils/handleInput';
+import { handleAmountChange } from './utils/handleAmountChange';
 
 export interface OffRampProps {
   account: GetAccountInfoResponse_Account;
@@ -334,59 +335,6 @@ export const OffRamp: FunctionComponent<OffRampProps> = ({
     onSaveBankAccount(account);
   };
 
-  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let value = event.target.value;
-
-    // Determine the decimal separator for the current locale
-    const decimalSeparator = (1.1).toLocaleString().substring(1, 2);
-
-    // Replace the non-local decimal separator with the local one
-    value = value.replace(
-      decimalSeparator === '.' ? ',' : '.',
-      decimalSeparator,
-    );
-
-    // Limit the number of decimal places to two
-    let result = value.replace(
-      new RegExp(`(\\${decimalSeparator}\\d{2})\\d+`),
-      '$1',
-    );
-
-    // Check if the result ends with a dot
-    if (result.endsWith(decimalSeparator)) {
-      const dotIndex = result.indexOf(decimalSeparator);
-      // Remove the dot if it is not the last character
-      if (dotIndex !== result.length - 1) {
-        result = result.substring(0, result.length - 1);
-      }
-    }
-
-    // Replace two or more leading zeros with a single zero
-    result = result.replace(/^00+/, '0');
-
-    // Check if the result starts with a zero and is an integer
-    if (
-      result.length > 1 &&
-      result.startsWith('0') &&
-      result.match(/^[0-9]*$/)
-    ) {
-      // Remove one leading zero (e.g., change "0123" to "123")
-      result = result.replace(/^0/, '');
-    }
-
-    // Remove all non-numeric characters except for the decimal separator
-    result = result.replace(new RegExp(`[^\\d${decimalSeparator}]`, 'g'), '');
-
-    // Don't update the state if the new value contains more than one decimal separator
-    if (
-      (result.match(new RegExp(`\\${decimalSeparator}`, 'g')) || []).length > 1
-    ) {
-      return;
-    }
-
-    setAmount(result);
-  };
-
   const bankAccountType =
     currency === 'GBP' ? 'instant Faster Payments' : 'SEPA Instant';
 
@@ -497,7 +445,9 @@ export const OffRamp: FunctionComponent<OffRampProps> = ({
                             type="text"
                             id="amount"
                             value={amount}
-                            onChange={handleAmountChange}
+                            onChange={(event) =>
+                              handleAmountChange(event, setAmount)
+                            }
                             disabled={false}
                           />
                         </div>
